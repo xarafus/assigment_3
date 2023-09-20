0cee29d34642481faf78c64c95e67e56ccd6a44a
xarafus <125128286+xarafus@users.noreply.github.com>
2023-09-20 12:08:47 +0600

    Update ass_3_3.py

--- ass_3_3.py
+++ ass_3_3.py
@@ -1,1 +1,19 @@
-
+sum = 0
+cnt = 0
+while True:
+    number = (input("Enter a number: "))
+    if number == "done":
+        break
+    try:
+        number = int(number)
+        sum += number
+        cnt += 1
+    except ValueError:
+        print("invalid input. enter a number")
+if cnt > 0:
+    average = sum / cnt
+else:
+    average = 0
+print("Sum of input numbers : ", sum)
+print("number of input : ", cnt)
+print("Average of input numbers : ", average)
